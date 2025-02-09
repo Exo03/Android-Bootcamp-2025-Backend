64b4c8d389571eb9cd3844ed110965011efa7ffd
Exo03 <118001156+Exo03@users.noreply.github.com>
2025-02-09 13:05:10 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/example/bootcamp/config/WebSecurityConfig.java
+++ src/main/java/com/example/bootcamp/config/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/api/users/register").permitAll()
                 .antMatchers("/api/users/username/{username}").permitAll()
+                .antMatchers("/api/authority/**").hasAnyAuthority("ROLE_ADMIN")
                 .antMatchers("/api/users/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
